6b168aaca51adcc02494d7e2f761897dc14487ca
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-07-09 14:10:50 +0200

    [Update]calma_grid

---
+++ calma_investment_flow/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "calma_investment_flow",
+    'summary': """
+        Proceso de inversión para crowfunding""",
+    'author': "Pedro Guirao",
+    'license': 'AGPL-3',
+    'website': "https://ingenieriacloud.com",
+    'category': 'Tools',
+    'version': '12.0.1.0.0',
+    'depends': ['sale_management','website_sale'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
