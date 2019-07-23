00848bfbfa1e3d5f3106d749824cf2b96aed359b
cubells <vicent@vcubells.net>
2019-07-23 20:00:56 +0200

    [IMP] calma_investment_flow: improve code and controller

--- calma_investment_flow/controllers/controllers.py
+++ calma_investment_flow/controllers/controllers.py
@@ -1,24 +1,17 @@
-
+from odoo import _, fields, http, tools
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 import json
 
-from odoo import fields, http, tools, _
-from odoo.http import request
-
-
-from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class CustomWebsiteSale(WebsiteSale):
 
     @http.route(['/shop/cart'], type='http', auth="public", website=True)
     def cart(self, access_token=None, revive='', **post):
         """
-        Main cart management + abandoned cart revival
-        access_token: Abandoned cart SO access token
-        revive: Revival method when abandoned cart. Can be 'merge' or 'squash'
+        Redirect to shop
         """
-        # Debe redirigir a shop sin más
-
         return request.redirect('/shop')
 
     @http.route(['/shop/cart/update'], type='http', auth="public",
@@ -47,50 +40,24 @@ class CustomWebsiteSale(WebsiteSale):
             add_qty=add_qty,
             set_qty=set_qty,
             product_custom_attribute_values=product_custom_attribute_values,
-            no_variant_attribute_values=no_variant_attribute_values
+            no_variant_attribute_values=no_variant_attribute_values,
         )
         return request.redirect("/shop/payment")
 
-    @http.route(['/shop/cart/update_json'], type='json', auth="public",
-                methods=['POST'], website=True, csrf=False)
-    def cart_update_json(self, product_id, line_id=None, add_qty=None,
-                         set_qty=None, display=True):
-        """This route is called when changing quantity from the cart or adding
-        a product from the wishlist."""
-        order = request.website.sale_get_order(force_create=1)
-        if order.state != 'draft':
-            request.website.sale_reset()
-            return {}
+    @http.route(['/shop/confirmation'], type='http', auth="public",
+                website=True)
+    def payment_confirmation(self, **post):
+        """ End of checkout process controller. Confirmation is basically seing
+        the status of a sale.order. State at this point :
 
-        value = order._cart_update(product_id=product_id, line_id=line_id,
-                                   add_qty=add_qty, set_qty=set_qty)
-
-        if not order.cart_quantity:
-            request.website.sale_reset()
-            return value
-
-        order = request.website.sale_get_order()
-        value['cart_quantity'] = order.cart_quantity
-        from_currency = order.company_id.currency_id
-        to_currency = order.pricelist_id.currency_id
-
-        if not display:
-            return value
-
-        value['website_sale.cart_lines'] = request.env[
-            'ir.ui.view'].render_template("website_sale.cart_lines", {
-            'website_sale_order': order,
-            # compute_currency deprecated (not used in view)
-            'compute_currency': lambda price: from_currency._convert(
-                price, to_currency, order.company_id, fields.Date.today()),
-            'date': fields.Date.today(),
-            'suggested_products': order._cart_accessories()
-        })
-        value['website_sale.short_cart_summary'] = request.env[
-            'ir.ui.view'].render_template("website_sale.short_cart_summary", {
-            'website_sale_order': order,
-            'compute_currency': lambda price: from_currency._convert(
-                price, to_currency, order.company_id, fields.Date.today()),
-        })
-        return value
-
+         - should not have any context / session info: clean them
+         - take a sale.order id, because we request a sale.order and are not
+           session dependant anymore
+        """
+        sale_order_id = request.session.get('sale_last_order_id')
+        print(sale_order_id)
+        if sale_order_id:
+            order = request.env['sale.order'].sudo().browse(sale_order_id)
+            return request.render("calma_grid.confirmation", {'order': order})
+        else:
+            return request.redirect('/shop')
