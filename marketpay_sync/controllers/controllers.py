2732f907bafaf909d2773832778ce9a57e2e036a
cubells <vicent@vcubells.net>
2019-07-24 12:30:11 +0200

    [FIX]: calma_grid: problem on uploading files

--- marketpay_sync/controllers/controllers.py
+++ marketpay_sync/controllers/controllers.py
@@ -1,7 +1,29 @@
+from odoo.http import request, route
 from odoo.addons.portal.controllers.portal import CustomerPortal
+
+import base64
 
 
 class CustomPortalDetails(CustomerPortal):
     MANDATORY_BILLING_FIELDS = CustomerPortal.MANDATORY_BILLING_FIELDS + \
-                              ["zipcode", "state_id", "vat", 'x_dni_front',
-                               'x_dni_back']
+                              ["zipcode", "state_id", "vat",
+                               "x_name_dni_front", "x_name_dni_back"]
+
+    @route(['/my/account'], type='http', auth='user', website=True)
+    def account(self, redirect=None, **post):
+        dni_front = False
+        dni_back = False
+        if post:
+            dni_front = post.pop('x_dni_front')
+            dni_back = post.pop('x_dni_back')
+        response = super(CustomPortalDetails, self).account(
+            redirect=redirect, **post)
+        if post:
+            partner = request.env.user.partner_id
+            partner.sudo().write({
+                'x_dni_back': base64.encodebytes(dni_back.read()),
+                'x_dni_front': base64.encodebytes(dni_front.read()),
+                'x_name_dni_back': dni_back.filename,
+                'x_name_dni_front': dni_front.filename,
+            })
+        return response
